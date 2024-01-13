a2aee78cd1ad3feb86529134ea9f387c47342398
Kristian Lewis <kristianedwardlewis@gmail.com>
2024-01-13 17:32:58 -0500

    Add new custom hook for keyboard event listener. Use new hook to play the keyboard

--- src/components/SoundBoard.jsx
+++ src/components/SoundBoard.jsx
@@ -1,29 +1,22 @@
-import { useState, useRef, useReducer } from 'react'
+import { useState, useRef, useReducer, useEffect } from 'react'
 import SelectedSoundOptions from './SelectedSoundOptions';
 import Keyboard from './keyboard/Keyboard';
 import soundReducer from './soundReducer';
-
-/*
-    Im gonna completely rework this soon. The accordian nonsense is annoying me, probably want different event handling for the keyboard, and maybe more options for continous noise and what not
-    
+import notes from '../notes';
+/*    
     Everything is being passed from here into the next component. The design has changed enough that this component should just be removed and the next component has taken its place
 
-    Im currently in the process of making major changes to the sounds reducer. Moving the currently selected sound into the reducer. This will also make the dedicated gain, wavetype, octave states redundant
-    I will focus on moving the reducer into its own file, and getting the state mangement hooked in, I may pass down setters and values differently.
+    Im currently in the process of making major changes to the sounds reducer.
+    I may pass down setters and values differently.
+    Octave, gain, waveType things may be removed. They can be derived from the soundModules reducer, so they don't have to be derived and passed down from this component
     */
-
-
 
 const SoundBoard = () => {
     const id = useRef(-1);
     const [audioContext, setAudioContext] = useState(() => {return new AudioContext()})
     //dispatch is named incorrectly, sounds is also a bad name. Maybe soundModules? and change the component to soundModule?
     const [soundModules, dispatchSound] = useReducer(soundReducer, {sounds : [], currentSoundId : -1, currentIndex : -1})
-    const [mouseMove, setMouseMove] = useState(null)
-    //Had to move this up a level
     const [controlsDisabled, setControlsDisabled] = useState(true);
-
-    //These all need to become apart of the sound reducer
 
     const handleSetCurrentSoundID = (id) => {
         dispatchSound({type : "setCurrentId", id : id})
@@ -49,16 +42,54 @@ const SoundBoard = () => {
             o.stop(audioContext.currentTime + 1)
         } 
     }
-    const handleClick = () => {
-        console.log(sounds)
+
+    /*================================================================
+
+    HANDLING KEYBOARD EVENTS AND MOUSE/POINTER EVENTS
+
+    This should never really be relevant for mobile/touch devices. Hopefully it does not interfere with either.
+    I used mouseEnter because from my understanding, pointer enter is only really helpful for funtionality that includes both mouse and touch devices. Im speciuifically targeting mice
+    
+    Maybe in the future Ill remove the mouseEnter/leave portion of this and just use a "switch" on the sound board that when switched on will listen for keyboard events
+    That might be easier to deal with in general, and also a better expereince for thew user.
+    I'll explore that idea when I add switches for showing the notes and corresponding phsyical keyboard key
+    ================================================================*/
+    const [mouseMove, setMouseMove] = useState(null)
+    const [mouseIn, setMouseIn] = useState(false);
+
+    const handleMouseEnter = () => {
+        setMouseIn(true)
     }
-    const handleThing = () =>  {
+    //These are pointer rather than mouse events because touch devices control the control knobs in the same way. This are used in the control knobs
+    const handlePointerLeave = () => {
+        setMouseIn(false)
         setMouseMove(null)
     }
-    // A new solution needs to be figured out for
+    //Had these as separate functions, but I may just remove this handle function. Not important to worry about
+    const handlePointerUp = () => {
+        setMouseMove(null)
+    }
+    useEffect(() => {
+        const keyUpFunction = (e) => {
+            if(mouseIn){
+                console.log("keyup");
+                let element = notes.find((element) => element.letter === e.key)
+                //console.log(soundModules.sounds)
+                if(element !== undefined){
+                    handleButtonClick({baseFrequency : element.baseFrequency})
+                }
+            }
+        }
+        window.addEventListener("keyup", keyUpFunction);
+
+        return () => {
+            window.removeEventListener("keyup", keyUpFunction);
+        }
+    },[mouseIn, soundModules])
+    
     return (
         <>
-        <div className = "soundBoard" onPointerMove = {mouseMove} onPointerLeave = {handleThing} onPointerUp = {handleThing}>
+        <div className = "soundBoard" onPointerMove = {mouseMove} onPointerLeave = {handlePointerLeave} onPointerUp = {handlePointerUp} onMouseEnter = {handleMouseEnter}>
             <div className = "settingsContainer">
                 {/*The majority of these setters will not do anything and should be removed */}
                 <SelectedSoundOptions 
@@ -69,7 +100,6 @@ const SoundBoard = () => {
                     currentSoundId = {soundModules.currentSoundId}//why are there two versions of this
                     handleAddNewSound = {handleAddNewSound}
                     sounds = {soundModules.sounds}
-                    handleClick = {handleClick}
                     waveType = {soundModules.currentIndex === -1 ? "sine" : soundModules.sounds[soundModules.currentIndex].waveType} 
                     octave = {soundModules.currentIndex === -1 ? 0 : soundModules.sounds[soundModules.currentIndex].octave} 
                     gain = {soundModules.currentIndex === -1 ? 0 : soundModules.sounds[soundModules.currentIndex].gain} 
@@ -84,4 +114,3 @@ const SoundBoard = () => {
     )
 }
 export default SoundBoard;
-//                <button onClick = {handleClick}>sounds</button>
